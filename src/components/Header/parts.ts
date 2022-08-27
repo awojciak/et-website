57f1e274185cee1bbe1e76ee151c7d548d58c940
Adrian <adrianwojciak@interia.pl>
2022-08-27 18:58:17 +0200

    basic styles beginning

--- src/components/Header/parts.ts
+++ src/components/Header/parts.ts
@@ -1,19 +1,51 @@
 import styled from "@suid/system/styled";
 
-export const Wrapper = styled('div')(({ theme }) => ({}));
+export const Wrapper = styled("div")(({ theme }) => ({
+  height: 100,
+  width: "100%",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  paddingInline: 40,
+  borderBottom: "2px solid black",
+  backgroundColor: "white",
+  position: "fixed",
+  top: 0,
 
-export const Logo = styled('img')(({ theme }) => ({
-    height: 80
+  [theme.breakpoints.down("md")]: {
+    paddingInline: 10,
+  },
 }));
 
-export const Item = styled('div')(({ theme }) => ({}));
+export const Logo = styled("img")(({ theme }) => ({
+  height: 80,
 
-export const ItemsWrapper = styled('div')(({ theme }) => ({}));
-
-export const ShortLogo = styled('img')(({ theme }) => ({
-    height: 80
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
 }));
 
-export const Hamburger = styled('img')(({ theme }) => ({}));
+export const Item = styled("div")(({ theme }) => ({}));
 
-export const HamburgerWrapper = styled('div')(({ theme }) => ({}));
+export const ItemsWrapper = styled("div")(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
+}));
+
+export const ShortLogo = styled("img")(({ theme }) => ({
+  height: 80,
+
+  [theme.breakpoints.up("md")]: {
+    display: "none",
+  },
+}));
+
+export const Hamburger = styled("img")(({ theme }) => ({}));
+
+export const HamburgerWrapper = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "none",
+  },
+}));
